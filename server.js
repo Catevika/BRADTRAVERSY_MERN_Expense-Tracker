8be57ff565aa1156a49f94b6f75d0550fffa28c3
Catevika <dominique.bello@outlook.fr>
2023-03-26 14:09:08 +0200

    Update pckgs &  Nodemon + Heroku out

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
-
-dotenv.config({ path: './config/config.env' });
+const cors = require('cors');
+require('dotenv').config({ path: './config/config.env' });
 
 connectDB();
 
@@ -13,15 +12,18 @@ const transactions = require('./routes/transactions');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-if(process.env.NODE_ENV === 'development') {
+
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use('/api/v1/transactions', transactions);
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
